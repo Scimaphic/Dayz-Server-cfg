6b53d3ff528f38a6b336e6ac51fcec50bc010e82
Maxwell_Phantom <maxwellorz@gmail.com>
2020-12-08 20:46:23 +0800

    Bug fixed

--- init.c
+++ init.c
@@ -1,29 +1,42 @@
 void main()
 {
-	Hive ce = CreateHive();
-	if (ce)
-		ce.InitOffline();
-
+	//INIT WEATHER BEFORE ECONOMY INIT------------------------
 	Weather weather = g_Game.GetWeather();
 
-	weather.GetOvercast().SetLimits(0.0, 1.0);
-	weather.GetRain().SetLimits(0.0, 1.0);
-	weather.GetFog().SetLimits(0.0, 0.25);
+	weather.MissionWeather(false);    // false = use weather controller from Weather.c
 
-	weather.GetOvercast().SetForecastChangeLimits(0.0, 0.2);
-	weather.GetRain().SetForecastChangeLimits(0.0, 0.1);
-	weather.GetFog().SetForecastChangeLimits(0.15, 0.45);
+	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.4, 0.6), 1, 0);
+	weather.GetRain().Set( 0, 0, 1);
+	weather.GetFog().Set( Math.RandomFloatInclusive(0.05, 0.1), 1, 0);
 
-	weather.GetOvercast().SetForecastTimeLimits(1800, 1800);
-	weather.GetRain().SetForecastTimeLimits(600, 600);
-	weather.GetFog().SetForecastTimeLimits(1800, 1800);
+	//INIT ECONOMY--------------------------------------
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
 
-	weather.GetOvercast().Set(Math.RandomFloatInclusive(0.0, 0.3), 0, 0);
-	weather.GetRain().Set(Math.RandomFloatInclusive(0.0, 0.2), 0, 0);
-	weather.GetFog().Set(Math.RandomFloatInclusive(0.0, 0.1), 0, 0);
+	//DATE RESET AFTER ECONOMY INIT-------------------------
+	int year, month, day, hour, minute;
+	int reset_month = 9, reset_day = 20;
+	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
-	weather.SetWindMaximumSpeed(15);
-	weather.SetWindFunctionParams(0.1, 0.3, 50);
+	if ((month == reset_month) && (day < reset_day))
+	{
+		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+	}
+	else
+	{
+		if ((month == reset_month + 1) && (day > reset_day))
+		{
+			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+		}
+		else
+		{
+			if ((month < reset_month) || (month > reset_month + 1))
+			{
+				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+			}
+		}
+	}
 }
 
 class CustomMission: MissionServer
@@ -37,6 +50,7 @@ class CustomMission: MissionServer
 		return m_player;
 	}
 
+
 	void addMags(PlayerBase player, string mag_type, int count)
 	{
 		if (count < 1)
@@ -46,118 +60,135 @@ class CustomMission: MissionServer
 
 		for (int i = 0; i < count; i++) {
 			mag = player.GetInventory().CreateInInventory(mag_type);
-			player.SetQuickBarEntityShortcut(mag, i + 1, true);
+			//player.SetQuickBarEntityShortcut(mag, i + 1, true);
 		}
 
 	}
 
-	EntityAI assault1Class(PlayerBase player)
+	EntityAI Assault_1(PlayerBase player)
 	{
 		EntityAI gun = player.GetHumanInventory().CreateInHands("M4A1_Black");
 		gun.GetInventory().CreateAttachment("M4_RISHndgrd_Black");
 		gun.GetInventory().CreateAttachment("M4_MPBttstck_Black");
 		gun.GetInventory().CreateAttachment("M4_Suppressor");
-		gun.GetInventory().CreateAttachment("M4_CarryHandleOptic");
-		addMags(player, "Mag_STANAG_30Rnd", 5);
+		gun.GetInventory().CreateAttachment("ACOGOptic");
+		player.GetInventory().CreateInInventory("M9A1_Bayonet");
+		gun.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+		gun.GetInventory().CreateAttachment("Mag_STANAGCoupled_30Rnd");
+		addMags(player, "Mag_STANAGCoupled_30Rnd", 4);
 
 		return gun;
 	}
-	EntityAI assault2Class(PlayerBase player)
+	EntityAI Assault_2(PlayerBase player)
 	{
 		EntityAI gun = player.GetHumanInventory().CreateInHands("AKM");
 		gun.GetInventory().CreateAttachment("AK_RailHndgrd_Black");
-		gun.GetInventory().CreateAttachment("ReflexOptic");
 		gun.GetInventory().CreateAttachment("AK_PlasticBttstck_Black");
+		gun.GetInventory().CreateAttachment("AK_Suppressor");
+		gun.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+		player.GetInventory().CreateInInventory("AK_Bayonet");
+		gun.GetInventory().CreateAttachment("Mag_AKM_Drum75Rnd");
 		addMags(player, "Mag_AKM_Drum75Rnd", 4);
 
 		return gun;
 	}
 
-	EntityAI shotgunClass(PlayerBase player)
+	EntityAI Shotgun_1(PlayerBase player)
 	{
 		EntityAI gun = player.GetHumanInventory().CreateInHands("Mp133Shotgun");
-		addMags(player, "AmmoBox_00buck_10rnd", 5);
+		addMags(player, "Ammo_12gaPellets", 8);
 
 		return gun;
 	}
 
 	/*
-	EntityAI smgClass(PlayerBase player)
+		EntityAI SMG_1(PlayerBase player)
 	{
-		EntityAI optic_attach
-		EntityAI gun = player.GetHumanInventory().CreateInHands("UMP45");
-		gun.GetInventory().CreateAttachment("PistolSuppressor");
-		optic_attach = gun.GetInventory().CreateAttachment("M68Optic");
-	    optic_attach.GetInventory().CreateAttachment( "Battery9V" );
-		gun.GetInventory().CreateAttachment("PistolSuppressor");
-        addMags(player, "Mag_UMP_25Rnd", 4);
+		EntityAI gun = player.GetHumanInventory().CreateInHands("Mp5k");
+		addMags(player, "Ammo_12gaPellets", 8);
 
 		return gun;
 	}
 	*/
+
+	EntityAI Sniper_1(PlayerBase player)
+	{
+		EntityAI optic_attach
+		EntityAI gun = player.GetHumanInventory().CreateInHands("Mosin9130");
+		gun.GetInventory().CreateAttachment("Mosin_Compensator");
+		player.GetInventory().CreateInInventory("Mosin_Bayonet");
+		gun.GetInventory().CreateAttachment("GhillieAtt_Woodland");
+		optic_attach = gun.GetInventory().CreateAttachment("PUScopeOptic");
+		addMags(player, "Ammo_762x54", 10);
+		return gun;
+	}
 	
 	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
 	{
-		//
-		TStringArray pants = {"Jeans_Black"};
-		TStringArray shoes = {"CombatBoots_Black"};
-		TStringArray yifu = {"HikingJacket_Black","HikingJacket_Red","HikingJacket_Blue","HikingJacket_Green"};
+		
+		TStringArray clothes = {"HikingJacket_Black","HikingJacket_Red","HikingJacket_Blue","HikingJacket_Green","RidersJacket_Black"};
 		TStringArray backpack = {"AliceBag_Black","AssaultBag_Black"};
-      		TStringArray vest = {"HighCapacityVest_Black"};
 		TStringArray drink = {"SodaCan_Cola","SodaCan_Kvass","SodaCan_Pipsi","SodaCan_Spite"};
-       		TStringArray food = {"Rice","SmallGuts","PowderedMilk","PeachesCan","Pear"};
-		TStringArray tool = {"Rope","Matchbox"};
-		//
+		
 		
 		player.RemoveAllItems();
-		
-		player.GetInventory().CreateInInventory(pants.GetRandomElement());
-		player.GetInventory().CreateInInventory(shoes.GetRandomElement());
-		player.GetInventory().CreateInInventory(yifu.GetRandomElement());
+
+
+		player.GetInventory().CreateInInventory(clothes.GetRandomElement());
 		player.GetInventory().CreateInInventory(backpack.GetRandomElement());
-		player.GetInventory().CreateInInventory(vest.GetRandomElement());
 		player.GetInventory().CreateInInventory(drink.GetRandomElement());
-		player.GetInventory().CreateInInventory(food.GetRandomElement());
 		player.GetInventory().CreateInInventory(drink.GetRandomElement());
-		player.GetInventory().CreateInInventory(food.GetRandomElement());
-		player.GetInventory().CreateInInventory(tool.GetRandomElement());
-		player.GetInventory().CreateInInventory("CanOpener");
+		player.GetInventory().CreateInInventory("TunaCan");
+		player.GetInventory().CreateInInventory("TunaCan");
+		player.GetInventory().CreateInInventory("Jeans_Black");
+		player.GetInventory().CreateInInventory("CombatBoots_Black");
+		player.GetInventory().CreateInInventory("HighCapacityVest_Black");
+		player.GetInventory().CreateInInventory("PetrolLighter");
+		player.GetInventory().CreateInInventory("Rope");
+		player.GetInventory().CreateInInventory("NVGHeadstrap");
+		player.GetInventory().CreateInInventory("NVGoggles");
         player.GetInventory().CreateInInventory("PersonalRadio");
-		player.GetInventory().CreateInInventory("MilitaryBelt");
-		player.GetInventory().CreateInInventory("PlateCarrierHolster");
-		player.GetInventory().CreateInInventory("NylonKnifeSheath");
 		player.GetInventory().CreateInInventory("CombatKnife");
 		player.GetInventory().CreateInInventory("TacticalGloves_Black");
+        player.GetInventory().CreateInInventory("CharcoalTablets");
 		player.GetInventory().CreateInInventory("M67Grenade");
-        player.GetInventory().CreateInInventory("Battery9V");
+		player.GetInventory().CreateInInventory("FlashGrenade");
+		player.GetInventory().CreateInInventory("M18SmokeGrenade_White");
 		player.GetInventory().CreateInInventory("Battery9V");
 		player.GetInventory().CreateInInventory("Battery9V");
-        player.GetInventory().CreateInInventory("AmmoBox_45ACP_25rnd");
-		player.GetInventory().CreateInInventory("AmmoBox_45ACP_25rnd");
-		player.GetInventory().CreateInInventory("AmmoBox_45ACP_25rnd");
-		player.GetInventory().CreateInInventory("AmmoBox_45ACP_25rnd");
-		player.GetInventory().CreateInInventory("PistolSuppressor");
-		player.GetInventory().CreateInInventory("Canteen");
+		player.GetInventory().CreateInInventory("Battery9V");
+		player.GetInventory().CreateInInventory("Battery9V");
+		player.GetInventory().CreateInInventory("PainKillerTablets");
 		player.GetInventory().CreateInInventory("OrienteeringCompass");
-		player.GetInventory().CreateInInventory("FNX45");
-		ItemBase rags = player.GetInventory().CreateInInventory("Rag");
-		rags.SetQuantity(4);
-		addMags(player, "Mag_FNX45_15Rnd", 3);
+		player.GetInventory().CreateInInventory("BandageDressing");
+
+		EntityAI blt = player.GetInventory().CreateInInventory("MilitaryBelt");
+		blt.GetInventory().CreateAttachment("PlateCarrierHolster");
+		blt.GetInventory().CreateAttachment("NylonKnifeSheath");
+		blt.GetInventory().CreateInInventory("Canteen");
+
+		EntityAI dgl = player.GetInventory().CreateInInventory("Deagle");
+		dgl.GetInventory().CreateAttachment("PistolSuppressor");
+		dgl.GetInventory().CreateAttachment("PistolOptic");
+		dgl.GetInventory().CreateAttachment("Mag_Deagle_9rnd");
+		addMags(player,"Mag_Deagle_9rnd",4);
 
 		EntityAI primary;
 		EntityAI axe = player.GetInventory().CreateInInventory("FirefighterAxe_Black");
+		EntityAI bdg = player.GetInventory().CreateInInventory("BandageDressing");
 	
 		switch (Math.RandomInt(0, 4)) {
-			case 0: primary = assault1Class(player); break;
-			case 1: primary = assault2Class(player); break;
-			case 2: primary = shotgunClass(player); break;
-			//case 3: primary = smgClass(player); break;
+			case 0: primary = Assault_1(player); break;
+			case 1: primary = Assault_2(player); break;
+			case 2: primary = Shotgun_1(player); break;
+			case 3: primary = Sniper_1(player); break;
 		}
 			
 		player.LocalTakeEntityToHands(primary);
 		player.SetQuickBarEntityShortcut(primary, 0, true);
-		player.SetQuickBarEntityShortcut(rags, 4, true);
-		player.SetQuickBarEntityShortcut(axe, 5, true);
+		player.SetQuickBarEntityShortcut(dgl, 1, true);
+		player.SetQuickBarEntityShortcut(bdg, 3, true);
+		player.SetQuickBarEntityShortcut(axe, 2, true);
 	}
 };
 
